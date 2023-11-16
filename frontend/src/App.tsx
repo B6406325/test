5e4355951d130c2c21737386867b81df5bbb5325
B6406325 <milindafook@gmail.com>
2023-11-16 17:54:14 +0700

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,19 @@ import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import About from './page/About';
+import Admin from './page/admin/Admin';
+import Member from './page/member/member';
+import Content from './page/content/content';
+import Login from './page/login/Login';
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Login/>}/>
         <Route path="about" element={<About />} />
+        <Route path='admin' element={<Admin/>}/>
+        <Route path='member' element={<Member/>}/>
+        <Route path='content' element={<Content/>}/>
       </Routes>
     </div>
   );
--- frontend/src/page/login/Login.tsx
+++ frontend/src/page/login/Login.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+import './Login.css'
+
 export default function Login(){
     return(
-        <></>
+        <div className='web-login'>
+            <div className='body-login'>
+                <div className='body-login-text'>เข้าสู่ระบบ</div>
+                <div className='input-email'></div>
+                <div className='input-password'></div>
+                <div className='reg-text'> ยังไม่มีบัญชีผู้ใช้งาน?<Link to=''><div className='reg-link'>สร้างบัญชีผู้ใช้งาน</div></Link>
+                </div>
+            </div>
+        </div>
     );
 }
