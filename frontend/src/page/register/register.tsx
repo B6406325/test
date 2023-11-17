042b6ec7170c855c1ebcc0df4e7e4c146260f91f
B6406325 <milindafook@gmail.com>
2023-11-17 21:02:05 +0700

    ui finish

--- frontend/src/page/register/register.tsx
+++ frontend/src/page/register/register.tsx
@@ -1,7 +1,31 @@
 import './Register.css'
+import { ConfigProvider , Button , Input} from 'antd';
 
 export default function Register(){
     return(
-        <h2>Register</h2>
+        <div className='web-reg'>
+            <ConfigProvider theme={{
+                components:{
+                    Button:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                        primaryColor: '#000000',
+                    },
+                    Input:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                    },
+                },
+            }}>
+            <div className='body-reg'>
+                <div className='body-reg-text'>สร้างบัญชีผู้ใช้งาน</div>
+                <div className='reg-username'><Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='ชื่อผู้ใช้งาน'></Input></div>
+                <div className='reg-email'><Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='อีเมล'></Input></div>
+                <div className='reg-pass'><Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='รหัสผ่าน'></Input></div>
+                <div className='reg-password'><Input style={{width:670,height:53,fontSize:25,fontFamily:'Mitr'}} placeholder='ยืนยันรหัสผ่าน'></Input></div>
+                <div className='reg-button'><Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary'>สร้างบัญชี</Button></div>
+            </div>
+            </ConfigProvider>
+        </div>
     );
 }
