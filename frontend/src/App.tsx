fde270afb67fe7e0c9def89fc352bc801f859e8e
B6406325 <milindafook@gmail.com>
2023-11-17 20:12:35 +0700

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './page/Home';
-import About from './page/About';
 import Admin from './page/admin/Admin';
-import Member from './page/member/member';
 import Content from './page/content/content';
 import Login from './page/login/Login';
+import Register from './page/register/register';
 export default function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path="about" element={<About />} />
         <Route path='admin' element={<Admin/>}/>
-        <Route path='member' element={<Member/>}/>
+        <Route path='register' element={<Register/>}/>
         <Route path='content' element={<Content/>}/>
       </Routes>
     </div>
--- frontend/src/page/login/Login.tsx
+++ frontend/src/page/login/Login.tsx
@@ -1,16 +1,32 @@
 import { Link } from 'react-router-dom';
 import './Login.css'
+import { ConfigProvider , Button , Input} from 'antd';
 
 export default function Login(){
     return(
         <div className='web-login'>
+            <ConfigProvider theme={{
+                components:{
+                    Button:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                        primaryColor: '#000000',
+                    },
+                    Input:{
+                        colorPrimary: '#F5CE00',
+                        algorithm: true,
+                    },
+                },
+            }}>
             <div className='body-login'>
                 <div className='body-login-text'>เข้าสู่ระบบ</div>
-                <div className='input-email'></div>
-                <div className='input-password'></div>
+                <div className='input-email'><Input style={{width:670,height:69,fontSize:25}} placeholder='Email'></Input></div>
+                <div className='input-password'><Input style={{width:670,height:69,fontSize:25}} placeholder='Password'></Input></div>
                 <div className='reg-text'> ยังไม่มีบัญชีผู้ใช้งาน?<Link to=''><div className='reg-link'>สร้างบัญชีผู้ใช้งาน</div></Link>
+                <div className='reg-button'><Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary'>เข้าสู่ระบบ</Button></div>
                 </div>
             </div>
+            </ConfigProvider>
         </div>
     );
 }
