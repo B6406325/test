3377436665ddda36341e73797a63f1b203016ca9
B6406325 <milindafook@gmail.com>
2023-11-19 18:29:19 +0700

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet ,BrowserRouter as Router} from 'react-router-dom';
 import Admin from './page/admin/Admin';
-import Content from './page/content/content';
+import Content from './page/movie/Movie';
 import Login from './page/login/Login';
 import Register from './page/register/register';
+import User from './page/user/user';
+import { Footer, Header } from 'antd/es/layout/layout';
+import Movies from './page/movie/Movie';
+import Payment from './page/payment/Payment';
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +16,8 @@ export default function App() {
         <Route path='/' element={<Login/>}/>
         <Route path='/admin' element={<Admin/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/content' element={<Content/>}/>
+        <Route path='/movie' element={<Movies/>}/>
+        <Route path='/payment' element={<Payment/>}/>
       </Routes>
     </div>
   );
--- frontend/src/page/admin/Admin.tsx
+++ frontend/src/page/admin/Admin.tsx
@@ -1,5 +1,64 @@
+import React, { useState } from 'react';
+import './admin.css'
+import { Button, ConfigProvider, Layout, Menu, MenuProps, theme } from 'antd';
+import { Content, Header } from 'antd/es/layout/layout';
+import { BrowserRouter, Link, Route, Routes, useNavigate} from 'react-router-dom';
+import Movies from '../movie/Movie';
+import User from '../user/user';
+
+const { Sider } = Layout;
+
+
 export default function Admin(){
+  const navigate = useNavigate();
+  function clickMovie(){
+    navigate("/movie");
+  }
+  function clickPayment(){
+    navigate("/payment");
+  }
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
     return(
-        <h1>Admin</h1>
+      <ConfigProvider theme={{
+        components:{
+            Button:{
+                colorPrimary: '#F5CE00',
+                algorithm: true,
+                primaryColor: '#000000',
+            },
+        },
+    }}>
+      <Layout style={{height: "100vh",backgroundColor:'#D9D9D9'}}>
+        <Sider width={300}>
+          <Menu style={{height:"100vh",backgroundColor:'#000000'}} mode='inline'>
+              <div className='admin-movie'>
+                <Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary' onClick={clickMovie}>Movies</Button>
+              </div>
+              <div className='admin-payment'>
+                <Button style={{fontSize: 25,width: 200,height:50,fontFamily:'Mitr'}} type='primary' onClick={clickPayment}>Payment</Button>
+              </div>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{height: 100,marginTop:10,marginLeft:10,marginRight:10,backgroundColor:"#FFFFFF"}}/>
+          <Content style={{ height: 100,marginTop:10,marginLeft:10,marginRight:10 }}>
+          <div
+              style={{
+                padding: 24,
+                minHeight: "100%",
+                background: colorBgContainer,
+              }}
+            >
+              <Routes>
+                <Route path='/' element={<User/>}/>
+              </Routes>
+            </div>
+          </Content>
+        </Layout>
+    </Layout>
+    </ConfigProvider>
     );
 }
